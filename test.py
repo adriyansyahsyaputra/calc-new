87a9d3d07982141cc68916b24e66342d18bd545e
Adriyansyah Syaputra <adriansyahputra2626@gmail.com>
2024-03-04 21:34:51 +0700

    New Calculator

--- test.py
+++ test.py
@@ -1,52 +1,24 @@
-#import modul Tkinter
-from tkinter import *
+import kivy
+from kivy.app import App
+kivy.require('1.11.0')
 
-# Fungsi menampilkan jendela utama
-def kalkulator(lihat, side):
-    mycalc = Frame(lihat, borderwidth=4, bd=4, bg="powder blue")
-    mycalc.pack(side=side, expand=YES, fill=BOTH)
-    return mycalc
+from kivy.uix.gridlayout import GridLayout
+from kivy.config import Config
+from kivy.uix.button import Button
 
-def tombol(lihat, side, text, command=None, bg='grey'):
-    mycalc = Button(lihat, text=text, command=command, bg=bg)
-    mycalc.pack(side=side, expand=YES, fill=BOTH)
-    return mycalc
+Config.set('graphics', 'resizable', 1)
 
-class aplikasi(Frame):
-    # Fungsi untuk penerapan tombol yang ada
-    def __init__(self):
-        Frame.__init__(self)
-        self.option_add('*Font', 'sans-serif')
-        self.pack(expand=YES, fill=BOTH)
-        self.master.title('Kalkulator')
-        
-        display = StringVar()
-        Entry(self, relief=FLAT, textvariable=display, bd=30, bg='light green').pack(side=TOP, expand=YES, fill=BOTH)
-        
-        for TombolAngka in ('123', '456', '789', '.0+', '/*-'):
-            nomor = kalkulator(self, TOP)
-            for a in TombolAngka:
-                tombol(nomor, LEFT, a, lambda mycalc=display, q=a: mycalc.set(mycalc.get() + q))
-                
-        for Tombol_C in (['C']):
-            hapus = kalkulator(self, RIGHT)
-            for c in Tombol_C:
-                tombol(hapus, RIGHT, c, lambda mycalc=display, q=c: mycalc.set(''), bg='green')
-                
-        SamaDengan = kalkulator(self,LEFT)
-        for samadengan in '=':
-            if samadengan == '=':
-                TombolSama = tombol(SamaDengan, LEFT, samadengan)
-                TombolSama.bind('<ButtonRelease-1>', lambda e,s=self, mycalc=display: s.hitung(mycalc), '+')
-            else:
-                TombolSama = tombol(SamaDengan, LEFT, samadengan, lambda mycalc=display, s=' %s ' % samadengan: mycalc.set(mycalc.get()+s))
-    # Fungsi Perhitungan
-    def hitung(self, display):
-        try:
-            display.set(eval(display.get()))
-        except:
-            display.set('ERROR')
-            
-if __name__ == ' __main__ ':
-    Tk.mainloop(aplikasi())
-    
+class CalcGridLayout(GridLayout):
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                self.display.text = str(eval(calculation))
+            except Exception:
+                self.display.text = 'Error'
+
+class CalculatorApp(App):
+    def build(self):
+         return CalcGridLayout()
+
+if __name__ == "__main__":
+    CalculatorApp().run()
